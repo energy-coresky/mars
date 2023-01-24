dbe59236c2f1b15e4c86ef3d9fc387d811d557ce
Energy <71418770+energy-coresky@users.noreply.github.com>
2023-01-24 20:16:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Mars
+
+This is DEV-MODE-plugin (Ware) to help create tests
+
+Status: _under development_
+
---
+++ conf.php
@@ -0,0 +1,10 @@
+<?php
+
+$plans = [
+    'view' => ['path' => __DIR__ . '/mvc'],
+    'cfg' => ['path' => __DIR__ . '/mvc'],
+    'app' => [
+        'type' => 'dev',
+        //'require' => 'SQLite3',
+    ],
+];
